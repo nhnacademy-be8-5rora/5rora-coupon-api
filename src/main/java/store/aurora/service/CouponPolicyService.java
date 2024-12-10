20eb21e760885dd8a117f8668a5b6f05d6ae9d41
adffdfewqefw <bbeekhw@gmail.com>
2024-12-10 17:35:30 +0900

    .

--- src/main/java/store/aurora/service/CouponPolicyService.java
+++ src/main/java/store/aurora/service/CouponPolicyService.java
@@ -1,8 +1,12 @@
 package store.aurora.service;
 
 import org.springframework.stereotype.Service;
+import store.aurora.entity.CouponPolicy;
 
 @Service
 public class CouponPolicyService {
 
+    public void createCouponPolicy(CouponPolicy couponPolicy) {
+
+    }
 }
