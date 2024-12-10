bc73fe7ec7e55db16c861b8acb8d92b14a92e757
adffdfewqefw <bbeekhw@gmail.com>
2024-12-10 16:00:58 +0900

    .

---
+++ src/main/java/store/aurora/index/IndexController.java
@@ -0,0 +1,13 @@
+package store.aurora.index;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @GetMapping("/api/users/test")
+    public String index(){
+        return "mememememem";
+    }
+}
