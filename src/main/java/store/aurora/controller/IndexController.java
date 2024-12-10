e9526fd34f0ec0f30ef33f4fb5d68adf351af571
adffdfewqefw <bbeekhw@gmail.com>
2024-12-10 16:05:21 +0900

    .

--- src/main/java/store/aurora/controller/IndexController.java
+++ src/main/java/store/aurora/controller/IndexController.java
@@ -1,6 +1,5 @@
-package store.aurora.index;
+package store.aurora.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
---
+++ src/main/java/store/aurora/controller/MakeCouponPolicyController.java
@@ -0,0 +1,5 @@
+package store.aurora.controller;
+
+public class MakeCouponPolicyController {
+
+}
