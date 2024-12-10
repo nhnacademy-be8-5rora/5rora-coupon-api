573dda3c8c9e6d7e24610bf4fae6a2024b670c75
adffdfewqefw <bbeekhw@gmail.com>
2024-12-10 16:51:45 +0900

    .

--- src/main/java/store/aurora/entity/Book.java
+++ src/main/java/store/aurora/entity/Book.java
@@ -4,10 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
 @Entity
+@Data
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
