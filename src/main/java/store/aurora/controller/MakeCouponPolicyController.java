ea63e32b7f25626466cc02e616f45c639c894885
adffdfewqefw <bbeekhw@gmail.com>
2024-12-11 09:47:36 +0900

    .

--- src/main/java/store/aurora/controller/MakeCouponPolicyController.java
+++ src/main/java/store/aurora/controller/MakeCouponPolicyController.java
@@ -1,22 +1,24 @@
 package store.aurora.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import store.aurora.entity.CouponPolicy;
 import store.aurora.service.CouponPolicyService;
 
 //관리자용 쿠폰 생성 및 배포용
-@Controller
+@RestController
 @RequestMapping("/admin")
 public class MakeCouponPolicyController {
-    @Autowired
-    private CouponPolicyService couponPolicyService;
 
+    @Autowired
+    private final CouponPolicyService couponPolicyService;
+
+    public MakeCouponPolicyController(CouponPolicyService couponPolicyService) {
+        this.couponPolicyService = couponPolicyService;
+    }
+
+    //쿠폰 정책 생성 폼을 제시
     @GetMapping("/coupon-policy")
     public String showCouponPolicyForm(Model model) {
         model.addAttribute("couponPolicy", new CouponPolicy());
@@ -28,4 +30,17 @@ public class MakeCouponPolicyController {
         couponPolicyService.createCouponPolicy(couponPolicy);
         return "redirect:/admin/coupon-policy-list"; // 쿠폰 정책 목록 페이지로 리디렉션
     }
+
+    //특정 유저에게 쿠폰을 주는 폼
+    @GetMapping("/coupon-distribution")
+    public String giveCouponToUserForm() {
+        return "admin/coupon-policy-list";
+    }
+
+    //관리자가 입력한 쿠폰을 주는 명령어(특정 한명에게 줄 수 있으며, 특정 조건을 충족한 유저들에게 쿠폰을 뿌릴 수 있도록 함)
+    @PostMapping("/coupon-distribution")
+    public String giveCouponToUser() {
+        return "admin/coupon-policy-list";
+    }
+
 }
