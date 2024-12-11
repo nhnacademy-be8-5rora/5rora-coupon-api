3873bd86edc386e0c1b0909cd2a3f9576183b713
adffdfewqefw <bbeekhw@gmail.com>
2024-12-11 17:46:42 +0900

    .

--- src/main/java/store/aurora/dto/RequestCouponPolicyDTO.java
+++ src/main/java/store/aurora/dto/RequestCouponPolicyDTO.java
@@ -2,12 +2,26 @@ package store.aurora.dto;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import store.aurora.entity.BookPolicy;
+import store.aurora.entity.CategoryPolicy;
+import store.aurora.entity.DiscountRule;
+import store.aurora.entity.SaleType;
 
 import java.time.LocalDateTime;
 
 @Data
 public class RequestCouponPolicyDTO {
     private String policyName;
+    private SaleType saleType;
+    private DiscountRule discountRule;
+    private CategoryPolicy categoryPolicy;
+    private BookPolicy bookPolicy;
 
+    public RequestCouponPolicyDTO(String policyName, SaleType saleType, DiscountRule discountRule) {
+        this.policyName = policyName;
+        this.saleType = saleType;
+        this.discountRule = discountRule;
+    }
 
 }
+
--- src/main/java/store/aurora/service/CouponPolicyService.java
+++ src/main/java/store/aurora/service/CouponPolicyService.java
@@ -26,14 +26,6 @@ public class CouponPolicyService {
         this.couponRepository = couponRepository;
     }
 
-    public void batchInsert(List<RequestCouponPolicyDTO> couponDTOList) throws CouponInsertException {
-        try {
-            couponPolicyRepository.saveAll(couponDTOList);
-        } catch (Exception e) {
-            throw new CouponInsertException("insertCoupon ERROR! 쿠폰 추가 메서드를 확인해주세요\n" + "Params : " + couponDTOList);
-        }
-    }
-
     //쿠폰 정책 생성
     public void couponPolicyCreate(@Valid RequestCouponPolicyDTO requestCouponPolicyDTO) {
 
