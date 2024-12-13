ccb7342a165e64909952c841dfb04af5afe0d863
adffdfewqefw <bbeekhw@gmail.com>
2024-12-13 17:40:10 +0900

    .

--- src/main/java/store/aurora/controller/AdminCouponController.java
+++ src/main/java/store/aurora/controller/AdminCouponController.java
@@ -1,10 +1,6 @@
 package store.aurora.controller;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
@@ -14,8 +10,6 @@ import store.aurora.dto.DiscountRuleDTO;
 import store.aurora.dto.RequestCouponDto;
 import store.aurora.dto.RequestCouponPolicyDTO;
 import store.aurora.service.CouponPolicyService;
-
-import java.util.stream.Collectors;
 
 //관리자용 쿠폰 생성 및 배포용
 //관리자를 통해서 새 쿠폰을 데이터베이스에 저장하기 때문에 유효성, 무결성 중요(@Validated 사용)
@@ -56,39 +50,5 @@ public class AdminCouponController {
 
         return ResponseEntity.ok("사용자쿠폰이 생성되었습니다.");
     }
-
-        /* 사용자쿠폰 삭제 (관리자/자동)
-        @DeleteMapping(value = "/coupon/delete/{userCouponId}")
-        public ResponseEntity<String> couponDelete(@PathVariable("userCouponId") String couponId) {
-            couponPolicyService.couponDelete(couponId);  // 실제 쿠폰 삭제 처리
-            return ResponseEntity.ok("사용자쿠폰이 삭제되었습니다.");
-        }
-    */
-
-    //    //todo 쿠폰 정책 생성 폼을 제시
-//    @GetMapping("/coupon-policy")
-//    public String showCouponPolicyForm(Model model) {
-//        model.addAttribute("couponPolicy", new CouponPolicy());
-//        return ResponseEntity.ok().body("쿠폰정보가 생성되었습니다."); // 쿠폰 정책 폼 뷰
-//    }
-
-//    //todo 관리자가 입력한 쿠폰 정책을 생성후 데이터베이스에 동기화
-//    @PostMapping("/coupon-policy")
-//    public ResponseEntity<String> createCouponPolicy(@ModelAttribute CouponPolicy couponPolicy) {
-//        couponPolicyService.createCouponPolicy(couponPolicy);
-//        return ResponseEntity.ok().body("쿠폰정보가 생성되었습니다."); // 쿠폰 정책 목록 페이지로 리디렉션
-//    }
-//
-////    //todo 특정 유저에게 특정 쿠폰정책을 가진 쿠폰을 주는 폼
-////    @GetMapping("/coupon-distribution")
-////    public String giveCouponToUserForm() {
-////        return "admin/coupon-policy-list";
-////    }
-//
-//    //todo 관리자가 입력한 쿠폰을 주는 명령어(특정 한명에게 줄 수 있으며, 특정 조건을 충족한 유저들에게 쿠폰을 뿌릴 수 있도록 함)
-//    @PostMapping("/coupon-distribution")
-//    public String giveCouponToUser() {
-//        return "admin/coupon-policy-list";
-//    }
 
 }
