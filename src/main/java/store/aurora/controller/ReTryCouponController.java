bdfb7ca399ea16ce941933f8008302b3b59fad60
adffdfewqefw <bbeekhw@gmail.com>
2024-12-13 15:15:36 +0900

    .

--- src/main/java/store/aurora/controller/ReTryCouponController.java
+++ src/main/java/store/aurora/controller/ReTryCouponController.java
@@ -1,10 +1,12 @@
 package store.aurora.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/retryCoupon")
+@RequiredArgsConstructor
 public class ReTryCouponController {
     /*todo 유저가 버튼을 누를시 해당 유저가 특정 쿠폰이 있는지
        확인 후 없을 경우 다시 쿠폰을 배포(회원 가입시 주는 쿠폰만 이에 대해 적용)
