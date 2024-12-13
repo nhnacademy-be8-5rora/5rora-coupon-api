fea0c55afe6926df08ca5892eeeadb24eb6a7449
adffdfewqefw <bbeekhw@gmail.com>
2024-12-13 17:39:11 +0900

    .

--- src/main/java/store/aurora/controller/AdminCouponController.java
+++ src/main/java/store/aurora/controller/AdminCouponController.java
@@ -7,6 +7,7 @@ import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import store.aurora.dto.AddPolicyDTO;
 import store.aurora.dto.DiscountRuleDTO;
@@ -17,6 +18,7 @@ import store.aurora.service.CouponPolicyService;
 import java.util.stream.Collectors;
 
 //관리자용 쿠폰 생성 및 배포용
+//관리자를 통해서 새 쿠폰을 데이터베이스에 저장하기 때문에 유효성, 무결성 중요(@Validated 사용)
 @RestController
 @RequestMapping("/admin")
 @RequiredArgsConstructor
@@ -24,28 +26,11 @@ public class AdminCouponController {
 
     private final CouponPolicyService couponPolicyService;
 
-// BindingResult 검증을 처리하는 메서드
-private ResponseEntity<String> handleValidationErrors(BindingResult bindingResult) {
-    if (bindingResult.hasErrors()) {
-        String errorMessage = bindingResult.getAllErrors().stream()
-                .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                .collect(Collectors.joining(", "));
-        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
-    }
-    return null; // 에러가 없으면 null 반환
-}
-
     // 쿠폰정책 생성 (관리자) -> 쿠폰 정책은 생성밖에 안됨(정책 수정, 삭제시 -> 이전에 해당 쿠폰을 가진 유저들이 피해를 볼 수 있음)
     //모든 사용자 쿠폰을 확인해서 해당 쿠폰 정책 ID가 있는지 파악한 후에 삭제, 수정 가능하도록 구현은 가능
     @PostMapping(value = "/coupon/create")
-    public ResponseEntity<String> couponPolicyCreate(@RequestBody @Valid RequestCouponPolicyDTO requestCouponPolicyDTO,
-                                                     BindingResult bindingResult) { //@Valid 유효 검증
-
-        // 유효성 검사(BindingResult -> 폼 데이터 검증후 오류 데이터 정보 담음
-        ResponseEntity<String> errorResponse = handleValidationErrors(bindingResult);
-        if (errorResponse != null) {
-            return errorResponse;
-        }
+    public ResponseEntity<String> couponPolicyCreate(@RequestBody @Validated RequestCouponPolicyDTO requestCouponPolicyDTO,
+                                                     BindingResult bindingResult) { //@Validated 유효 검증(무결성)
 
         DiscountRuleDTO discountRuleDTO = requestCouponPolicyDTO.getDiscountRuleDTO();
         AddPolicyDTO addPolicyDTO = requestCouponPolicyDTO.getAddPolicyDTO();
@@ -57,13 +42,7 @@ private ResponseEntity<String> handleValidationErrors(BindingResult bindingResul
 
     // 사용자쿠폰 수정 (관리자)
     @PutMapping(value = "/coupon/update/")
-    public ResponseEntity<String> couponUpdate(@RequestBody @Valid RequestCouponDto requestCouponDto,
-                                               BindingResult bindingResult) {
-        // 유효성 검사
-        ResponseEntity<String> errorResponse = handleValidationErrors(bindingResult);
-        if (errorResponse != null) {
-            return errorResponse;
-        }
+    public ResponseEntity<String> couponUpdate(@RequestBody @Validated RequestCouponDto requestCouponDto) {
 
         couponPolicyService.couponUpdate(requestCouponDto);  // 실제 쿠폰 수정 처리
         return ResponseEntity.ok("사용자쿠폰이 수정되었습니다.");
@@ -71,13 +50,7 @@ private ResponseEntity<String> handleValidationErrors(BindingResult bindingResul
 
     //todo 관리자가 입력한 쿠폰을 주는 명령어(특정 한명에게 줄 수 있으며, 특정 조건을 충족한 유저들에게 쿠폰을 뿌릴 수 있도록 함)
     @PostMapping("/coupon/distribution")
-    public ResponseEntity<String> couponCreate(@RequestBody @Valid RequestCouponDto requestCouponDto,
-                                                   BindingResult bindingResult) {
-
-        ResponseEntity<String> errorResponse = handleValidationErrors(bindingResult);
-        if (errorResponse != null) {
-            return errorResponse;
-        }
+    public ResponseEntity<String> couponCreate(@RequestBody @Validated RequestCouponDto requestCouponDto) {
 
         couponPolicyService.userCouponCreate(requestCouponDto);
 
