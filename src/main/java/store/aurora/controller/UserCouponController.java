3f77ec49261226ed459f2ab4e9db30ed9ef812da
adffdfewqefw <bbeekhw@gmail.com>
2024-12-27 10:54:07 +0900

    명세서 작성 및 테스트 코드 보완

--- src/main/java/store/aurora/controller/UserCouponController.java
+++ src/main/java/store/aurora/controller/UserCouponController.java
@@ -3,7 +3,10 @@ package store.aurora.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import store.aurora.service.CouponService;
 
 import java.util.List;
